0a2e484049b84da61f993ceb0d39ebd1854269ce
samuel zaťovič <szatovic@gmail.com>
2019-10-20 10:02:44 +0200

    Hotový program

---
+++ Open-Lab-02.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_02._4
+{
+    class Program
+    {
+        public static int GetLegsCount(int chickens, int cows, int pigs)
+        {
+            int legs = (chickens * 2) + (cows * 4) + (pigs * 4);
+            return legs;
+        }
+    }
+}
